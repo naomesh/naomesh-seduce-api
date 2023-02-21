8dedccaf946e2b70f72e667b401dafdc37629d9d
Lucas ROURET <lucas.rouretperso@gmail.com>
2023-02-21 10:50:54 +0100

    fix production

--- src/app.service.ts
+++ src/app.service.ts
@@ -166,7 +166,7 @@ export class AppService {
     //return a DataPayload
     const rawData = [];
     for (let i = 0; i < data.results.length; i++) {
-      rawData.push(data.results[i].series[0].values[0]);
+      rawData.push(data.results[i].series[0].values);
     }
 
     return new DataPayload(
